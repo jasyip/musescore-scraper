4ab4966de1e15b429aba47b6a5c296b5ccef0c0c
Jason Yip <jasonyip1234567@gmail.com>
2021-09-23 08:16:04 -0500

    [FIX]: Better URL checking

--- musescore_scraper/helper.py
+++ musescore_scraper/helper.py
@@ -1,6 +1,11 @@
 from urllib.parse import urlparse
+import warnings
 
 def _valid_url(url: str) -> bool:
     final_url = urlparse(url + '/' * int(not url.endswith('/')))
+    if final_url.netloc != "musescore.com":
+        warnings.warn('Network location is not "musescore.com".')
+    elif re.fullmatch(r"\W+", final_url.path):
+        warnings.warn('URL path is empty.')
     return (all([final_url.scheme, final_url.netloc, final_url.path])
             and '.' in final_url.netloc)
